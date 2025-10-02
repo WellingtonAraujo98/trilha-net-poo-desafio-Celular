d53677957abf18b949d9d302d95d545a9dce440a
WellingtonAraujo98 <wellington.a14@hotmail.com>
2025-10-02 07:44:46 -0300

    desafio Celular feito

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+
+        public Smartphone(string numero, string modelo, string imei, int memoria )
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            
         }
 
         public void Ligar()
@@ -16,9 +22,18 @@ namespace DesafioPOO.Models
             Console.WriteLine("Ligando...");
         }
 
+
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+        public void FazerLigacao()
+        {
+            Console.WriteLine("Fazendo ligação...");
+        }
+        public void DesligarAparelho()
+        {
+            Console.WriteLine("Desligando...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
 ﻿using DesafioPOO.Models;
 
+//Precisei mudar para NET 8.0 para rodar no meu Visual Studio
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.Clear();
+
+
+        Console.WriteLine($"Teste Nokia: \n");
+        Smartphone nokia = new Nokia("123456789", "Nokia", "Modelo X", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+        nokia.FazerLigacao();
+        nokia.ReceberLigacao();
+        nokia.DesligarAparelho();
+        Console.WriteLine();
+
+
+        Console.WriteLine($"Teste Iphone: \n");
+        Smartphone iphone = new Iphone("987654321", "Apple", "iPhone 13", 128);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+        iphone.FazerLigacao();
+        iphone.ReceberLigacao();
+        iphone.DesligarAparelho();
+    }
+}
